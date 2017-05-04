9eaddba28bc33256d5695931aee85ca9a2ef666f
lambhunter0 <george.theophanous@hotmail.com>
2017-05-04 13:23:36 -0400

    made background and player paddle

---
+++ BrickBeakerGame/Assets/scripts/backgroundMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundMove : MonoBehaviour {
+
+	//this script makes the background scroll to give it a cool efffect. we can deactivate this whenever
+	//im just implementing something from an assignment cause I think it looks cool
+	public float speed = 0.5f;
+
+	void Update(){
+		Vector2 backgroundMvt = new Vector2 (0, (Time.time * speed)%1);
+		GetComponent<Renderer> ().material.mainTextureOffset = backgroundMvt;
+	}
+}
---
+++ BrickBeakerGame/Assets/scripts/playerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float speed;
+	
+	void Update () {
+		moveInput ();
+	}
+
+	void moveInput(){
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			//moveright
+			//for now use translate, we might use physical movements later to allow for different kinds of powerups/powerdowns
+			transform.Translate(new Vector2(1,0)*speed*Time.deltaTime,Space.World);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			//moveleft
+			transform.Translate(new Vector2(-1,0)*speed*Time.deltaTime,Space.World);
+		}
+	}
+}
